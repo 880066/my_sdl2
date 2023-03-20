06c74b7fd10c82c4250f5c9c2451bbd4f479d652
Nazım Çoban <nzmcoban2000@hotmail.com>
2023-03-20 12:58:32 +0300

    siyah olan arkaplan kırmızı olarak değiştirildi

--- main.cpp
+++ main.cpp
@@ -10,7 +10,13 @@ int main(int argc, char* argv[]) {
 
     SDL_Window *window = SDL_CreateWindow("Title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN); // SDL_WINDOW_SHOWN meaning is ignore this flag
 
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);  // arka planý window isimli pencere için oluþturuyoruz.
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);  // arka plani window isimli pencere icin olusturuyoruz.
+
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);  //arka plani kirmizi ayarladik. rgb ve alpha degeri. varsayilan olan siyahtir , deger atansa bile default olan temizlenmeli
+
+    SDL_RenderClear(renderer);  // default arka plani temizledik
+
+    SDL_RenderPresent(renderer);  // ayarlanmis arka plan verisini çizdirdik
 
     SDL_Delay(3000);  // 3 saniye boyunca ekranda kalsýn
 
