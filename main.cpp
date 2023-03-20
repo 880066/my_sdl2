c201f9b28123f0ca990bafb6de242ab4e9c07287
Nazım Çoban <nzmcoban2000@hotmail.com>
2023-03-20 17:51:22 +0300

    header dosyası ve class ile birlikte parametrik bir şekilde oyun penceresi açıldı

--- main.cpp
+++ main.cpp
@@ -1,24 +1,21 @@
-#include "SDL.h"
-#include "SDL_image.h"
-#include <stdio.h>
-#include <Windows.h>
+#include "Game.h"
 
+Game* game = nullptr;
 
 int main(int argc, char* argv[]) {
    
-    SDL_Init(SDL_INIT_VIDEO);  //Initialize the SDL library.
+    game = new Game();
 
-    SDL_Window *window = SDL_CreateWindow("Title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN); // SDL_WINDOW_SHOWN meaning is ignore this flag
+    game->init("BirchEngine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
 
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);  // arka plani window isimli pencere icin olusturuyoruz.
+    while (game->running())
+    {
+        game->handleEvents();
+        game->update();
+        game->render();
+    }
 
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);  //arka plani kirmizi ayarladik. rgb ve alpha degeri. varsayilan olan siyahtir , deger atansa bile default olan temizlenmeli
-
-    SDL_RenderClear(renderer);  // default arka plani temizledik
-
-    SDL_RenderPresent(renderer);  // ayarlanmis arka plan verisini çizdirdik
-
-    SDL_Delay(3000);  // 3 saniye boyunca ekranda kalsýn
+    game->clean();
 
     return 0;
 }
